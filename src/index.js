7f6214198a283952c4f8fbfee2bbe3951479fb5a
Alexey Guskov <lexag@mail.ru>
2016-08-11 10:46:20 +0300

    links added and a bit refactor

--- src/index.js
+++ src/index.js
@@ -9,58 +9,58 @@ import Model from './model';
 
 let _renders = 0;
 
-@observer
-class App extends React.Component {
-  render() {
-    var store = this.props.store;
-
-    return <div>
-      <div>Renders count: { ++_renders }
-        <button onClick={() => store.add() }>
-          Add children
-        </button>
-      </div>
-      <div className='children'>
-        <List items={store.items} />
-      </div>
+const App = observer(props =>
+  <div>
+    <div>Renders count: { ++_renders }
+      <button onClick={() => props.store.add() }>
+        Add children
+      </button>
     </div>
-  }
-}
-
-@observer
-class List extends React.Component
-{
-  render()
-  {
-    return <div className='children'>
-      {this.props.items.map((item, i) =>
-        <Item key={i} model={item} />
-      )}
+    <div className='children'>
+      <List items={props.store.items} />
     </div>
-  }
-}
+  </div>
+)
 
-@observer
-class Item extends React.Component {
-  render() {
-    const model = this.props.model;
+const List = observer(props =>
+  <div className='children'>
+    {props.items.map((item, i) =>
+      <Item key={i} model={item} />
+    )}
+  </div>
+)
 
-    return <div className='checklist'>
-      <div className='header'>
-        <input type='checkbox' checked={model.checked} onChange={a => model.checked = a.target.checked} />
-        <span className="created">{model.created.toLocaleTimeString()}</span>
-        <input value={model.name} onChange={a => model.name = a.target.value} />
-        <button onClick={() => model.remove()}>
-          Delete
-        </button>
-        <button onClick={() => model.add()}>
-          Add children
-        </button>
-      </div>
-      <List items={model.items} />
+const Item = observer(props => {
+  const model = props.model;
+
+  return <div className='checklist'>
+    <div className='header'>
+      <Checkbox link={link(model, 'checked')} />
+      <span className="created">{model.created.toLocaleTimeString()}</span>
+      <Input link={link(model, 'name')} />
+      <button onClick={() => model.remove()}>
+        Delete
+      </button>
+      <button onClick={() => model.add()}>
+        Add children
+      </button>
     </div>
-  }
-}
+    <List items={model.items} />
+  </div>
+})
+
+const link = (target, name) => ({
+  set: (val) => target[name] = val,
+  get: () => target[name]
+})
+
+const Checkbox = observer(props =>
+  <input type='checkbox' checked={props.link.get()} onChange={a => props.link.set(a.target.checked)} />
+)
+
+const Input = observer(props =>
+  <input value={props.link.get()} onChange={a => props.link.set(a.target.value)} />
+)
 
 const store = new Model();
 
